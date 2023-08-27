5525e53da14453f30bc769316f56031398d21210
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2023-08-27 18:02:49 +0300

    Minimal Giraffe App From https://github.com/giraffe-fsharp/Giraffe/issues/497\#issuecomment-968400589

---
+++ demo-apps/MinimalApp/Program.fs
@@ -0,0 +1,10 @@
+﻿open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Hosting
+open Giraffe
+
+let builder = WebApplication.CreateBuilder()
+builder.Services.AddGiraffe() |> ignore
+let app = builder.Build()
+
+app.UseGiraffe(text "Hello World!")
+app.Run()
