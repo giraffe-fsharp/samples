1e5db2b6d460c3ba07421053fb3d3b0f7f55157a
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2023-08-31 17:00:04 +0300

    Use routing

--- demo-apps/MinimalApp/Program.fs
+++ demo-apps/MinimalApp/Program.fs
@@ -5,6 +5,10 @@ open Giraffe
 let builder = WebApplication.CreateBuilder()
 builder.Services.AddGiraffe() |> ignore
 let app = builder.Build()
+let webApp =
+    choose [
+        route "/ping"   >=> text "pong"
+        route "/"       >=> text "Hello World" ]
 
-app.UseGiraffe(text "Hello World!")
+app.UseGiraffe(webApp)
 app.Run()
